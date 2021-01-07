af644802dfb7d39d03371dfc64d2d7136fe3dccb
Gjorgji Noveski <37218494+Gjorgji-Noveski@users.noreply.github.com>
2021-01-08 00:29:30 +0100

    Made cmd callable and changed input argument types
    Added the ability to call the script using the terminal (the input can be a single text file or a dir containing folders of entity categories, and inside them txt files), can define what kind of delimiter to use, added one more check if the word is empty after using re.sub, added automatic naming of the entity types depending on the input folder

--- convertingToBIOscheme.py
+++ convertingToBIOscheme.py
@@ -1,64 +1,131 @@
 import re
+import plac
+import os
+from pathlib import Path
 
-entities = set()
-taggedCorpus = open("C:/Users/Gjorgji Noveski/Desktop/Files for my Spacy work/Files for evaluating a model/new sentences, after sentence 850+.csv", mode='w', encoding='UTF-8')
-with open("C:/Users/Gjorgji Noveski/Desktop/Files for my Spacy work/Files for evaluating a model/new sentences, after sentence 850+.txt",
-          mode='r', encoding='UTF-8') as sentFile:
-    lines = sentFile.readlines()
-for line in lines:
-    words = line.split(" ")
-    foundStartBracket = False
-    if words[-1] == '':
-        words = words[:-1]
-    for idx, word in enumerate(words, 1):
-        hasComma = False
+DELIMITER = '\t'
+FILE_EXTENSION = '.tsv'
 
-        # checking to see if the word has a coma
-        if word.isspace() or word == '':
-            continue
-        if word[-1] == ',':
-            hasComma = True
-        word = re.sub("[,();`“”]", "", word)
 
-        if idx == len(words):
-            word = re.sub("[.\n]", "", word)
-        if len(word) == 0:
-            continue
+###############
 
-        # checking to see if the word is a reference to another research, {} brackets are used in AllSents.txt
-        # instead of []
-        if word[0] == '{':
-            taggedCorpus.write("[,O\n")
-            word = re.sub("[{]", "", word)
+# TODO: ima mnogu lini posledovatelni so
+#  .  0
+#  .  0
+#  .  0
+#  ....
 
-        # Ako e samo eden zbor vo entitetot
-        if word.find("[") != -1 and word.find("]") != -1:
-            cleanWord = re.sub("[\[\]]", "", word)
-            taggedCorpus.write(cleanWord + ",B-DeepLearning\n")
+###############
 
-        # ako e pochetniot zbor vo entitetot
-        elif word.find("[") != -1:
-            cleanWord = re.sub("[\[\]]", "", word)
-            taggedCorpus.write(cleanWord + "," + "B-DeepLearning\n")
-            foundStartBracket = True
+@plac.annotations(
+    inputFileOrDir=(
+    "The input text file or dir where category dirs are, to be converted into a BIO scheme", "option", "i", Path),
+    outputDir=("Output path dir.", "option", "o", Path))
+def main(inputFileOrDir=None, outputDir=None):
+    global DELIMITER, FILE_EXTENSION
+    if inputFileOrDir is None or outputDir is None:
+        print('Please enter txt-file/dir Path, and outputDir Path')
+        raise SystemExit(1)
+    if not os.path.exists(inputFileOrDir):
+        print('Please enter valid input path')
+        raise SystemExit(1)
+    if not os.path.exists(outputDir):
+        os.makedirs(outputDir)
 
-        # ako e krajniot zbor vo entitetot
-        elif word.find("]") != -1:
-            cleanWord = re.sub("[\[\]]", "", word)
-            taggedCorpus.write(cleanWord + "," + "I-DeepLearning\n")
+    if os.path.isdir(inputFileOrDir):
+        for cat in os.listdir(inputFileOrDir):
+            allLine = []
+            entCat = os.path.join(inputFileOrDir, cat)
+            for sentFile in os.listdir(entCat):
+                allLine += getLines(os.path.join(entCat, sentFile))
+            outputCatDir = os.path.join(outputDir, cat)
+            if not os.path.exists(outputCatDir):
+                os.makedirs(outputCatDir)
+            outputFile = os.path.join(outputCatDir, cat + FILE_EXTENSION)
+            entityName = cat.replace('model', '').replace(' ', '_')
+            convert(allLine, outputFile, entityName)
+            print('Converted %s annotated dataset to %s' % (cat, FILE_EXTENSION))
+    else:
+        outputFile = os.path.join(outputDir, os.path.basename(inputFileOrDir)[:-4] + FILE_EXTENSION)
+        convert(inputFileOrDir, outputFile)
+        print('Converted %s to %s' % (os.path.basename(inputFileOrDir), os.path.basename(outputFile)))
+
+
+def getLines(inputFile):
+    with open(inputFile, mode='r', encoding='utf-8')as f:
+        lines = f.readlines()
+    return lines
+
+
+def convert(inputFileOrDir, outputDir, entityName):
+    global DELIMITER
+    if not type(inputFileOrDir) is list:
+        with open(inputFileOrDir, mode='r', encoding='UTF-8') as sentFile:
+            lines = sentFile.readlines()
+    else:
+        lines = inputFileOrDir
+    with open(outputDir, mode='w', encoding='UTF-8') as taggedCorpus:
+        for line in lines:
+            words = line.replace('\t', ' ').split(' ')
             foundStartBracket = False
+            if words[-1] == '':
+                words = words[:-1]
+            for idx, word in enumerate(words, 1):
+                hasComma = False
 
-        # ako e vnatreshen zbor vo entitetot
-        elif foundStartBracket:
-            taggedCorpus.write(word + "," + "I-DeepLearning\n")
-        #
-        elif word[-1] == '}':
-            word = re.sub("[}]", "", word)
-            taggedCorpus.write(word + "," + "O\n")
-            taggedCorpus.write("],O\n")
-        else:
-            taggedCorpus.write(word + "," + "O\n")
-        if hasComma:
-            taggedCorpus.write(',' + ',' + "O\n")
-    taggedCorpus.write(".,O\n")
-taggedCorpus.close()
+                # checking to see if the word has a coma
+                if word.isspace() or word == '':
+                    continue
+                if word[-1] == ',':
+                    hasComma = True
+                word = re.sub(r'[,();`“”]', '', word)
+
+                if idx == len(words):
+                    word = re.sub(r'[.\n]', '', word)
+
+                if len(word) == 0:
+                    continue
+
+                # References in text are in '[ ]' brackets, our entities will be in '{ }' brackets
+                if word[0] == '[':
+                    taggedCorpus.write('[' + DELIMITER + '\n')
+                    word = re.sub(r'[\[]', '', word)
+
+                if len(word) == 0:
+                    continue
+
+                # Ako e samo eden zbor vo entitetot
+                # Nekogas mozhe i vakvo da primi: {perceptron}-based, ke gi trgne zagradite i posle dvata zbora ke bidat kako eden entitet.
+                if word.find('{') != -1 and word.find('}') != -1:
+                    cleanWord = re.sub(r'[{}]', '', word)
+                    taggedCorpus.write(cleanWord + DELIMITER + 'B-%s\n' % entityName)
+
+                # ako e pochetniot zbor vo entitetot
+                elif word.find('{') != -1:
+                    cleanWord = re.sub(r'[{]', '', word)
+                    taggedCorpus.write(cleanWord + DELIMITER + 'B-%s\n' % entityName)
+                    foundStartBracket = True
+
+                # ako e krajniot zbor vo entitetot
+                elif word.find('}') != -1:
+                    cleanWord = re.sub(r'[}]', '', word)
+                    taggedCorpus.write(cleanWord + DELIMITER + 'I-%s\n' % entityName)
+                    foundStartBracket = False
+
+                # ako e vnatreshen zbor vo entitetot
+                elif foundStartBracket:
+                    taggedCorpus.write(word + DELIMITER + 'I-%s\n' % entityName)
+                #
+                elif word[-1] == ']':
+                    word = re.sub(r'[\]]', '', word)
+                    taggedCorpus.write(word + DELIMITER + 'O\n')
+                    taggedCorpus.write(']' + DELIMITER + 'O\n')
+                else:
+                    taggedCorpus.write(word + DELIMITER + 'O\n')
+                if hasComma:
+                    taggedCorpus.write(',' + DELIMITER + 'O\n')
+            taggedCorpus.write('.' + DELIMITER + 'O\n')
+
+
+if __name__ == '__main__':
+    plac.call(main)
