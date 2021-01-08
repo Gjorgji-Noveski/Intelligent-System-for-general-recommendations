f405c76e73189be23146aa0ce64c8a27cd168a1c
Gjorgji Noveski <37218494+Gjorgji-Noveski@users.noreply.github.com>
2021-01-09 00:02:30 +0100

    Missing O after writing [, cleaned writing empty sents

--- convertingToBIOscheme.py
+++ convertingToBIOscheme.py
@@ -7,16 +7,6 @@ DELIMITER = '\t'
 FILE_EXTENSION = '.tsv'
 
 
-###############
-
-# TODO: ima mnogu lini posledovatelni so
-#  .  0
-#  .  0
-#  .  0
-#  ....
-
-###############
-
 @plac.annotations(
     inputFileOrDir=(
     "The input text file or dir where category dirs are, to be converted into a BIO scheme", "option", "i", Path),
@@ -66,7 +56,9 @@ def convert(inputFileOrDir, outputDir, entityName):
         lines = inputFileOrDir
     with open(outputDir, mode='w', encoding='UTF-8') as taggedCorpus:
         for line in lines:
-            words = line.replace('\t', ' ').split(' ')
+            # za da ne dobijam prazni rechenici vo krajniot json, zatoa brisham prazno_mestoTOCKAprazno_Mesto
+
+            words = re.sub(r'\W\.\W', '', line).replace('\t', ' ').split(' ')
             foundStartBracket = False
             if words[-1] == '':
                 words = words[:-1]
@@ -78,24 +70,25 @@ def convert(inputFileOrDir, outputDir, entityName):
                     continue
                 if word[-1] == ',':
                     hasComma = True
+                # ako koristam \W za se shto ne e zbor, ke mi se trgnat i bitnite raboti kako , . [] {}. Ne znam kako da napravam regex iskluchok za niv
                 word = re.sub(r'[,();`“”]', '', word)
 
                 if idx == len(words):
-                    word = re.sub(r'[.\n]', '', word)
+                    word = re.sub(r'[\.\n]', '', word)
 
                 if len(word) == 0:
                     continue
 
                 # References in text are in '[ ]' brackets, our entities will be in '{ }' brackets
                 if word[0] == '[':
-                    taggedCorpus.write('[' + DELIMITER + '\n')
+                    taggedCorpus.write('[' + DELIMITER + 'O\n')
                     word = re.sub(r'[\[]', '', word)
 
                 if len(word) == 0:
                     continue
 
-                # Ako e samo eden zbor vo entitetot
                 # Nekogas mozhe i vakvo da primi: {perceptron}-based, ke gi trgne zagradite i posle dvata zbora ke bidat kako eden entitet.
+                # Ako e samo eden zbor vo entitetot
                 if word.find('{') != -1 and word.find('}') != -1:
                     cleanWord = re.sub(r'[{}]', '', word)
                     taggedCorpus.write(cleanWord + DELIMITER + 'B-%s\n' % entityName)
@@ -128,4 +121,5 @@ def convert(inputFileOrDir, outputDir, entityName):
 
 
 if __name__ == '__main__':
-    plac.call(main)
+    # plac.call(main)
+    main(r'C:\Users\Gjorgji Noveski\Desktop\Files for my Spacy work\Annotated dataset for different NER models', r'C:\Users\Gjorgji Noveski\Desktop\test')
