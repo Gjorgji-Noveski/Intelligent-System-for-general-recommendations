9121840ea0e9d72d47a0c0f024b012220a845fa8
Gjorgji Noveski <37218494+Gjorgji-Noveski@users.noreply.github.com>
2021-01-09 00:04:07 +0100

    Made to open files in UTF-8

--- tsvToJson.py
+++ tsvToJson.py
@@ -1,12 +1,11 @@
 import json
 import logging
-nnn=1
+
+
 def tsv_to_json_format(input_path, output_path, unknown_label):
-    global nnn
     try:
-        f = open(input_path, 'r')  # input file
-        #fp = open(output_path, 'w')  # output file
-        fw = open("C:/Users/Gjorgji Noveski/Desktop/Files for my Spacy work/Corpora - text with annotated , { }/sents for evaluation a model.txt",'w')
+        f = open(input_path, 'r', encoding='utf-8')  # input file
+        fp = open(output_path, 'w', encoding='utf-8')  # output file
         data_dict = {}
         annotations = []
         label_dict = {}
@@ -31,11 +30,8 @@ def tsv_to_json_format(input_path, output_path, unknown_label):
                             label_dict[entity].append(d)
                 start += len(word) + 1
 
-
             else:
                 data_dict['content'] = s
-                content = s
-
                 s = ''
                 label_list = []
                 for ents in list(label_dict.keys()):
@@ -56,26 +52,11 @@ def tsv_to_json_format(input_path, output_path, unknown_label):
                     label = {}
                     label['label'] = [entities[0]]
                     label['points'] = entities[1:]
-
                     annotations.append(label)
-                # ('I like London and Berlin.',
-                #      [(7, 13, 'LOC'), (18, 24, 'LOC')])
                 data_dict['annotation'] = annotations
-                THEREALstartlistendlist= []
-                for tmp in data_dict['annotation']:
-                    ent = tmp['label'][0]
-                    startEndList = []
-                    for lblDic in tmp['points']:
-                        THESTART = lblDic['start']
-                        THEEND = lblDic['end']
-                        startEndList.append((THESTART,THEEND,ent))
-                    THEREALstartlistendlist.extend(startEndList)
-                print((content,THEREALstartlistendlist))
-
-                print("\n\n\n")
                 annotations = []
-                #json.dump(data_dict, fp)
-                #fp.write('\n')
+                json.dump(data_dict, fp)
+                fp.write('\n')
                 data_dict = {}
                 start = 0
                 label_dict = {}
@@ -84,5 +65,19 @@ def tsv_to_json_format(input_path, output_path, unknown_label):
         return None
 
 
-tsv_to_json_format("C:/Users/Gjorgji Noveski/Desktop/Files for my Spacy work/Corpora - text with annotated , { }/taggedCorpus.tsv",
-                   'C:/Users/Gjorgji Noveski/Desktop/Files for my Spacy work/Corpora - text with annotated , { }/taggedCorpus.json', 'abc')
+tsv_to_json_format(
+    r"C:\Users\Gjorgji Noveski\Desktop\test\activation function model\activation function model.tsv",
+    r"C:\Users\Gjorgji Noveski\Desktop\test\activation function model\activation function model.json",
+    'abc')
+tsv_to_json_format(
+    r"C:\Users\Gjorgji Noveski\Desktop\test\architecture type model\architecture type model.tsv",
+    r"C:\Users\Gjorgji Noveski\Desktop\test\architecture type model\architecture type model.json",
+    'abc')
+tsv_to_json_format(
+    r"C:\Users\Gjorgji Noveski\Desktop\test\building blocks model\building blocks model.tsv",
+    r"C:\Users\Gjorgji Noveski\Desktop\test\building blocks model\building blocks model.json",
+    'abc')
+tsv_to_json_format(
+    r"C:\Users\Gjorgji Noveski\Desktop\test\dataset model\dataset model.tsv",
+    r"C:\Users\Gjorgji Noveski\Desktop\test\dataset model\dataset model.json",
+    'abc')
