da24dd9272222ecdb9a899c3648b44f1ff66e989
Gjorgji Noveski <37218494+Gjorgji-Noveski@users.noreply.github.com>
2021-01-10 23:26:28 +0100

    Added option for splitting the json into train/test bin files

--- JsonToSpacyBinary.py
+++ JsonToSpacyBinary.py
@@ -1,36 +1,60 @@
 import plac
-import logging
 import json
 import pickle
+import random
+import os
 
-@plac.annotations(input_file=("Input file", "option", "i", str), output_file=("Output file", "option", "o", str))
-def main(input_file=None, output_file=None):
-    try:
-        training_data = []
-        with open(input_file, 'r', encoding='utf-8') as f:
-            lines = f.readlines()
 
-        for line in lines:
-            data = json.loads(line)
-            text = data['content']
-            entities = []
+@plac.annotations(input_file=("Input file", "option", "i", str),
+                  output_dir=("Output directory", "option", "o", str),
+                  train_test_split=(
+                  'Real value from 0.0 - 1.0  to represent how much of the json data to be used for training.'
+                  ' Eg. -split 0.6, will write 2 bin files, first one will have 60% of the content of the json file,'
+                  ' and the second will get the remainder.', 'option', 'split', float))
+def main(input_file=None, output_dir=None, train_test_split=None):
+    if not os.path.isfile(input_file):
+        print('Please specify an input file')
+        raise SystemExit(1)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    with open(input_file, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+    if train_test_split is None:
+        outputFileName = os.path.basename(input_file).split('.')[0] + '.bin'
+        convert(lines, os.path.join(output_dir, outputFileName))
 
-            for annotation in data['annotation']:
-                points = annotation['points']
-                label = annotation['label'][0]
-                if not isinstance(points, list):
-                    points = [points]
-                for point in points:
-                    entities.append((point['start'], point['end'] + 1, label))
+    else:
+        split = int(len(lines) * train_test_split)
+        random.shuffle(lines)
+        train_data = lines[:split]
+        test_data = lines[split:]
+        convert(train_data, os.path.join(output_dir, 'train_data.bin'))
+        convert(test_data, os.path.join(output_dir, 'test_data.bin'))
 
-            training_data.append((text, {"entities": entities}))
-        with open(output_file, 'wb') as fp:
-            pickle.dump(training_data, fp)
 
-    except Exception as e:
-        logging.exception("Unable to process " + input_file + "\n" + "error = " + str(e))
-        return None
+def convert(lines, output_file):
+    training_data = []
+    for line in lines:
+        data = json.loads(line)
+        text = data['content']
+        entities = []
+
+        for annotation in data['annotation']:
+            points = annotation['points']
+            label = annotation['label'][0]
+            if not isinstance(points, list):
+                points = [points]
+            for point in points:
+                entities.append((point['start'], point['end'] + 1, label))
+
+        training_data.append((text, {"entities": entities}))
+
+    with open(output_file, 'wb') as fp:
+        pickle.dump(training_data, fp)
+
 
 
 if __name__ == '__main__':
     plac.call(main)
+
+
