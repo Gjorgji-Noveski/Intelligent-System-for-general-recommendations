a3eacb4e5ea9a349428ed8fb571b0ccd780e2ebb
Gjorgji Noveski <37218494+Gjorgji-Noveski@users.noreply.github.com>
2021-01-10 12:57:18 +0100

    Made code append more than one point per label

--- JsonToSpacyBinary.py
+++ JsonToSpacyBinary.py
@@ -4,35 +4,33 @@ import json
 import pickle
 
 @plac.annotations(input_file=("Input file", "option", "i", str), output_file=("Output file", "option", "o", str))
-
 def main(input_file=None, output_file=None):
     try:
         training_data = []
-        with open(input_file, 'r') as f:
+        with open(input_file, 'r', encoding='utf-8') as f:
             lines = f.readlines()
 
         for line in lines:
             data = json.loads(line)
             text = data['content']
             entities = []
+
             for annotation in data['annotation']:
-                point = annotation['points'][0]
-                labels = annotation['label']
-                if not isinstance(labels, list):
-                    labels = [labels]
+                points = annotation['points']
+                label = annotation['label'][0]
+                if not isinstance(points, list):
+                    points = [points]
+                for point in points:
+                    entities.append((point['start'], point['end'] + 1, label))
 
-                for label in labels:
-                    entities.append((point['start'], point['end'] + 1 ,label))
-
-
-            training_data.append((text, {"entities" : entities}))
-
-
+            training_data.append((text, {"entities": entities}))
         with open(output_file, 'wb') as fp:
             pickle.dump(training_data, fp)
 
     except Exception as e:
         logging.exception("Unable to process " + input_file + "\n" + "error = " + str(e))
         return None
+
+
 if __name__ == '__main__':
     plac.call(main)
