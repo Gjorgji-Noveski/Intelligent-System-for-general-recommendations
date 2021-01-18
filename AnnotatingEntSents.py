428d62317461d7e25e5d2e2d075b1cc0ca29179c
Gjorgji Noveski <37218494+Gjorgji-Noveski@users.noreply.github.com>
2021-01-18 02:27:14 +0100

    Made to add space if entity boundary is non space char

--- AnnotatingEntSents.py
+++ AnnotatingEntSents.py
@@ -1,44 +1,23 @@
 import os
 import re
-
-EXTRACTED_ENT_SENTS_PATH = r'C:\Users\Gjorgji Noveski\Desktop\Files for my Spacy work\Extracted Entity Sents, cleaned fig,Table,et al'
-ANNOTATED_ENT_SENTS_PATH = r'C:\Users\Gjorgji Noveski\Desktop\Files for my Spacy work\Annotated dataset for different NER models'
+from WordSetAndMappings import DATASET, ARCHITECTURE_TYPE, ACTIVATION_FUNC, BUILDING_BLOCKS
+EXTRACTED_ENT_SENTS_PATH = r'C:\Users\Gjorgji Noveski\Desktop\nov spacy test\Extracted Entity Sents, one phrase entity'
+ANNOTATED_ENT_SENTS_PATH = r'C:\Users\Gjorgji Noveski\Desktop\nov spacy test\Annotated dataset AMA SO PRAZNO MESTO'
 
 """
-Probaj namesto site variacii na auto encoder, samo autoencoder da anotirash, deka ako zborot e variational auto encoder, a bara za auto encoder, toj ednash ke go anotira so auto encoder, a posle koga ke bara za variational pak za nego ke go anotira.
-Mozhno da se desi i slednovo:
-bla bla [Variational [auto encoder]] bla bla
-i da vide vaka anotirano.
 Mozhe samo da barame za auto encoder pa posle da izvlecheme ako zborot pred nego e denoising, variational itn.
 istoto mozhe i za neural network da bide, primer samo neural network da barame i posle da izvlecheme i za boltzman machine
-
-Broj na sloevi od sekoj gradben blok ke se dobie so proverka na Building_blocks za sekoj otkrien entitet, ke proveri pred nego sto brojka sedi ako voopshto sedi
 """
-DATASET = ['number', 'subjects', 'dataset', 'patients', 'images', 'analyzed', 'downloaded', 'retrieved', 'created',
-           'corpus', 'corpora', 'files']
-ARCHITECTURE_TYPE = ['perceptron', 'feed forward', 'radial basis network', 'recurrent neural network',
-                     'long short-term memory', 'long short term memory', 'long-short term memory',
-                     'long shortterm memory', 'gated recurrent unit', 'auto encoder', 'markov chain',
-                     'hopfield network', 'boltzman machine', 'deep belief network', 'convolutional neural network',
-                     'deconvolutional neural network', 'convolutional inverse graphics network',
-                     'probabilistic neural network', 'generative adversarial network', 'liquid state machine',
-                     'extreme learning machine', 'echo state network', 'deep residual network', 'kohonen network',
-                     'support vector machine', 'neural turing machine', 'rnn', 'lstm', 'gru', 'rbm', 'gan', 'lsm',
-                     'elm', 'esn', 'drn', 'svm', 'ntm']
-ACTIVATION_FUNC = ['linear activation function', 'rectified linear unit', 'sigmoid', 'gaussian error linear unit',
-                   'softmax', 'hyperbolic tangent function', 'softsign function', 'exponential linear unit',
-                   'leaky rectified linear unit', 'relu', 'gelu', 'tanh', 'elu', 'lrelu', 'leaky relu']
-BUILDING_BLOCKS = ['fully connected layer', 'fully-connected layer', 'recurrent layer', 'pooling layer',
-                   'convolutional layer', 'convolution layer', 'deconvolutional layer', 'deconvolution layer',
-                   'dropout layer', 'softmax layer', 'subsampling layer', 'gru layer']
+
+# TODO: DODADOV CNN nemashe,Anotiraj gi pak
+##
+
 CATEGORIES = {'dataset model': DATASET, 'architecture type model': ARCHITECTURE_TYPE,
               'activation function model': ACTIVATION_FUNC, 'building blocks model': BUILDING_BLOCKS}
 
 """
 Annotates all the corpora and saves them in their dedicated folder (based on entity category, a.k.a model)
 """
-
-
 def annotateByCategory(inputPath, outputPath):
     global CATEGORIES
     for file in os.listdir(inputPath):
@@ -52,23 +31,40 @@ def annotateByCategory(inputPath, outputPath):
             sentences = set()
             for line in allLines:
                 line = line.replace('{', '').replace('}', '')
+
                 # Extracting only the sentences that i managed to annotate
                 annotatedAnEntity = False
                 for entity in CATEGORIES[category]:
-                    regex = r'(?<=\()' + entity + r's?(?=\))|\b' + entity + r's?\b'
+                    regex = r'(?<=\()' + entity + r's?(?=\))|(?<=\s)' + entity + r'(?=\s)'
 
                     # Using regex to search for singular and plural of entities and if they also appear in '( )' brackets.
                     matchObjIter = re.finditer(regex, line, flags=re.IGNORECASE)
                     for counter, match in enumerate(matchObjIter):
-                        annotatedAnEntity = True
-
                         # using counter so i can annotate more than 1 entity in one sentence, without it the array positions won't be correct for the 2nd annotation
+                        annotatedAnEntity = True
                         line = line[:match.start() + (2 * counter)] + '{' + match.group() + '}' + line[match.end() + (2 * counter):]
+
+
+                # Ovie 2 for loops, ako ima neshto sto ne e prazno mesto pred entitetot i posle, mu dadavam prazno mesto, za da ima alignment so spacy tokenizer
+                timesOpeningBracket = line.count('{')
+                foundIdx = 0
+                for i in range(timesOpeningBracket):
+                    foundIdx = line.find('{', foundIdx +1)
+                    if line[foundIdx - 1] != ' ':
+
+                        line = line[:foundIdx] + ' ' + line[foundIdx:]
+
+                timesClosingBracket = line.count('}')
+                foundIdx = 0
+                for i in range(timesClosingBracket):
+                    foundIdx = line.find('}', foundIdx + 1)
+                    if line[foundIdx + 1] != ' ':
+                        line = line[:foundIdx + 1] + ' ' + line[foundIdx+1:]
                 if annotatedAnEntity:
                     sentences.add(line)
             with open(annotatedFile, encoding='utf-8', mode='a')as fw:
                 fw.writelines(sentences)
-            print("Extracted " + category + ' - ' + file)
+            print("Annotated " + category + ' - ' + file)
 
 
 annotateByCategory(EXTRACTED_ENT_SENTS_PATH, ANNOTATED_ENT_SENTS_PATH)
