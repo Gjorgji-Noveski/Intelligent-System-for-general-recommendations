5bda61d50f76af3fcdaa38ea1263805b325191cf
Gjorgji Noveski <37218494+Gjorgji-Noveski@users.noreply.github.com>
2021-02-07 22:58:14 +0100

    Added ability to be called by command line

--- GettingEntitySents.py
+++ GettingEntitySents.py
@@ -2,24 +2,13 @@ import spacy
 import os
 import os.path
 import fitz
-import re
-from pathlib import Path
+import plac
+from Preprocessing import preprocessSinglePdfPage, checkAlreadyProcessedFile
 
-NLP_MODEL_PATH = r'C:\Users\Gjorgji Noveski\Desktop\Files for my Spacy work\NLP models and bin training ' \
-                 r'data\Incremental epochs,850sents, decaying dropout(0.6,0.35,0.00003),batch compounding(32,128,1.001),ischisteni sents' \
-                 r'\65 epochs on 850 sents'
-NLP_MODEL_PATH_PARSER = r'C:\Users\Gjorgji Noveski\Desktop\65 epochs on 850 sents'
-
-MODEL_NAME = NLP_MODEL_PATH.split(os.path.sep)[-2] + ' ' + NLP_MODEL_PATH.split(os.path.sep)[-1]
-
-nlp = spacy.load(NLP_MODEL_PATH_PARSER)
-# nlp.add_pipe(nlp.create_pipe('sentencizer'))
-nlp.max_length = 1200000
+NLP_MODEL_PATH_PARSER = r'C:\Users\Gjorgji Noveski\Desktop\nov spacy test\22 epochs on 850 sents 77fscore'
 
 DOWNLOADED_PDFS_PATH = r"C:\Users\Gjorgji Noveski\PycharmProjects\SciBooksCrawler\SciBooks\Downloaded PDFs"
-OUTPUT_FOLDER = r'C:\Users\Gjorgji Noveski\Desktop\Files for my Spacy work\Extracted Entity Sents, cleaned fig,Table,et al'
-fileNames = []
-pdfPathNames = {}
+OUTPUT_FOLDER = r'C:\Users\Gjorgji Noveski\Desktop\nov spacy test\Extracted Entity Sents, one phrase entity'
 
 ''' ------------------------ '''
 
@@ -28,90 +17,71 @@ pdfPathNames = {}
 ''' ------------------------ '''
 
 
+def extractEntitySents(pdf, outputDir, countFolder, countFile, file, characterLimit, nlp):
+    # Getting table of content titles
+    # Mozhno podobruvanje, baraj go naslovot samo na stranata koja e navedeno
+    tocTitles = [element[1] for element in pdf.getToC()]
+    extractedEntSentFileName = str(countFolder) + '00' + str(countFile) + '_' + file[:5] + '_' + file[-15:-4] + '.txt'
+    EXTRACTED_ENTITY_SENTS_PATH = os.path.join(outputDir, extractedEntSentFileName)
+    wholePdfText = ''
+    for page in pdf:
 
-def extractEntitySents(pdfs_path, output_folder):
-    global fileNames, pdfPathNames
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-    with open(os.path.join(OUTPUT_FOLDER, "Model used here.txt"), mode='w', encoding='utf-8')as mf:
-        mf.write(MODEL_NAME)
+        pageTextInLines = preprocessSinglePdfPage(page, tocTitles)
+
+        wholePdfText += ' '.join(pageTextInLines) + ' '
+
+    # Poradi toa shto Imam 8GB RAM, moram da go secham dokumentot koj mu go davam na spacy
+    # inache mozhno e da imam allocation error
+    # 100,000 karakteri = 1GB za spacy, ama go zgolemiv malce pojshe
+    splitPdfDocument = [wholePdfText[i:i + characterLimit] for i in range(0, len(wholePdfText), characterLimit)]
+    del wholePdfText
+    entitySents = set()
+    with open(EXTRACTED_ENTITY_SENTS_PATH, mode='a', encoding='UTF-8') as entSentFile:
+        for chunk in splitPdfDocument:
+            doc = nlp(chunk)
+            for ent in doc.ents:
+                entitySents.add(ent.sent.text)
+        entSentFile.writelines('\n'.join(entitySents))
+    print('Extracted: ' + extractedEntSentFileName)
+
+@plac.annotations(
+    model_path=('Path to a Spacy model that will be used for extracting named entities', 'option', 'm', str),
+    pdfs_path=('Directory containing categories and inside each category are PDF files.', 'option', 'i', str),
+    output_dir=('Output directory where all the extracted sentences that contain an entity will be saved.', 'option', 'o', str),
+    character_limit=('Maximum number of characters a single preprocessed pdf chunk will have.', 'option', 'l', int)
+)
+def main(model_path, pdfs_path, output_dir, character_limit=3000000):
+    """
+    This script takes as input a directory that contains other directories labeled by the category of PDFs
+    that are inside them.
+    Ex. Medicine_category -> contains PDFs related to the field of medicine
+    Engineering_category -> contains PDFs related to field of engineering
+
+    After processing the PDF files using a Spacy model it extracts the sentences that contain named entities.
+    It saves the found sentences in .txt from each PDF file to the path set by the output_folder parameter.
+
+    The character_limit parameter is used to check the number of characters of a PDF file after it has been preprocessed.
+    This is because Spacy's NER needs around 1GB of memory for every 100,000 characters and someone might have less
+    memory required to process a whole PDF file. So this parameter adds the option to split the preprocessed PDF
+    into parts that are <= character_limit. Default value is 3,000,000, suitable for any large PDF file you may find
+    in the wild.
+    """
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    nlp = spacy.load(model_path)
+    nlp.max_length = 3000000
     for countFolder, folder in enumerate(os.listdir(pdfs_path), 1):
         FILE_CATEGORY_PATH = os.path.join(pdfs_path, folder)
+        print(folder)
         for countFile, file in enumerate(os.listdir(FILE_CATEGORY_PATH), 1):
             if file[-4:] != '.pdf':
                 continue
-
-            # Checks if file is already processed by file name file size (sometimes 2 different pdfs have same name)
             PDF_PATH = os.path.join(FILE_CATEGORY_PATH, file)
-            if file not in fileNames:
-                fileNames.append(file)
-                pdfPathNames[file] = PDF_PATH
-            elif Path(pdfPathNames[file]).stat().st_size == Path(PDF_PATH).stat().st_size:
-                print('Info: Found duplicate file, skipping processing it')
+            if checkAlreadyProcessedFile(PDF_PATH, file):
                 continue
-
             pdf = fitz.open(PDF_PATH)
-            # Getting table of content titles
-            # Mozhno podobruvanje, baraj go naslovot samo na stranata koja e navedeno
-            # mnogu poedinechni sluchaevi ima shto nekoi tekst da se chisti/sredi
-            tocTitles = [element[1] for element in pdf.getToC()]
-            extractedEntSentFileName = str(countFolder) + '00' + str(countFile) + '_' + file[:5] + '_' + file[-15:-4] + '.txt'
-            EXTRACTED_ENTITY_SENTS_PATH = os.path.join(output_folder, extractedEntSentFileName)
-
-            wholePdfText = ''
-
-            for page in pdf:
-                """
-                Filters any line from a page that starts with "Fig." or "Table " because usually those word indicate annotation for a figure/table.
-                Also filters empty or lines filled with whitespace.
-                """
-                pageText = re.sub("-\n", "", page.getText().replace('\xa0', ' '))
-                pageTextInLines = pageText.split("\n")
-                pageTextInLines = [line.strip() for line in pageTextInLines if not line.isspace() and line != '' and line[:4] != 'Fig.' and line[:6] != 'Table ']
+            extractEntitySents(pdf, output_dir, countFolder, countFile, file, character_limit, nlp)
 
 
-                # checking if the first 2 sentences contain the "et al." part so it can be removed
-                if len(pageTextInLines) > 1:
-                    if 'et al' in pageTextInLines[0]:
-                        pageTextInLines.remove(pageTextInLines[0])
-                    elif 'et al' in pageTextInLines[1]:
-                        pageTextInLines.remove(pageTextInLines[1])
-
-                """
-                Deletes the first 2 sentences of a page if they contain the title of that chapter that is found in Table of Contents
-                and also if one of the sentences is numeric (a.k.a, page number)
-                """
-                for tocTitle in tocTitles:
-                    if len(pageTextInLines) > 2:
-                        tocTitle = tocTitle.strip()
-                        if tocTitle in pageTextInLines[0]:
-                            pageTextInLines.remove(pageTextInLines[0])
-
-                        elif tocTitle in pageTextInLines[1]:
-                            pageTextInLines.remove(pageTextInLines[1])
-
-                        if pageTextInLines[0].isnumeric():
-                            pageTextInLines.remove(pageTextInLines[0])
-
-                        elif pageTextInLines[1].isnumeric():
-                            pageTextInLines.remove(pageTextInLines[1])
-
-
-                wholePdfText += ' '.join(pageTextInLines) + ' '
-
-            # Poradi toa shto Imam 8GB RAM, moram da go secham dokumentot koj mu go davam na spacy
-            # inache mozhno e da imam allocation error
-            # 100,000 karakteri = 1GB za spacy, ama go zgolemiv malce pojshe
-            splitPdfDocument = [wholePdfText[i:i + 1100000] for i in range(0, len(wholePdfText), 1100000)]
-            del wholePdfText
-            entitySents = set()
-            with open(EXTRACTED_ENTITY_SENTS_PATH, mode='a', encoding='UTF-8') as entSentFile:
-                for chunk in splitPdfDocument:
-                    doc = nlp(chunk)
-                    for ent in doc.ents:
-                        entitySents.add(ent.sent.text)
-                entSentFile.writelines('\n'.join(entitySents))
-            print('Extracted: ' + extractedEntSentFileName)
-
-
-extractEntitySents(DOWNLOADED_PDFS_PATH, OUTPUT_FOLDER)
+if __name__ == '__main__':
+    plac.call(main)
