095f69beea4351e3a04835c900a9f5b44a777a57
Gjorgji Noveski <37218494+Gjorgji-Noveski@users.noreply.github.com>
2021-02-08 22:41:53 +0100

    Removed one redundant argument when calling the script

--- EvaluatingAModel.py
+++ EvaluatingAModel.py
@@ -3,6 +3,7 @@ from spacy.scorer import Scorer
 import os.path
 import pickle
 import plac
+import spacy
 
 # TODO: Careful NOT to evaluate a model on the SAME sentences it was trained
 # With this function i'm making sentences in the format
@@ -12,36 +13,40 @@ import plac
 #     ('I like London and Berlin.',
 #      [(7, 13, 'LOC'), (18, 24, 'LOC')])
 # ]
-# so i can get NER precision/recall/fscore of the model using the Scorer.score function
-
-"""
-This script writes evaluation results only for the entity recognizer
-"""
-
 
 @plac.annotations(
-    models_dir=('Path to a directory where spacy models will be used for evaluation', 'option', 'm', str),
-    input_file=('An input spacy binary file from which evaluation sentences are taken from', 'option', 'i', str),
-    output_dir=(
-    "Output directory where a .txt file containing entity evaluation results will be written", "option", "o", str))
-def main(models_dir=None, input_file=None, output_dir=None):
+    models_dir=('Path to a directory where spacy NER_models will be used for evaluation', 'option', 'm', str),
+    input_file=('An input spacy binary file from which evaluation sentences are taken from', 'option', 'i', str)
+)
+def main(models_dir=None, input_file=None):
+    """
+    This script writes evaluation results only for the NER pipeline of the model.
+    It takes a directory containing sub directiories of many Spacy models and saves the evaluation results inside the
+    models directory.
+    The binary input file is specified so the script know on which sentences to evaluate.
+    """
     if not os.path.isdir(models_dir):
-        print('Please specify a directory to spacy models')
+        print('Please specify a directory to spacy NER_models')
         raise SystemExit(1)
     if not os.path.isfile(input_file):
         print('Please specify an input file')
         raise SystemExit(1)
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
     evalSents = makingEvaluationSents(input_file)
-    outputFile = os.path.join(output_dir, "Evaluation results.txt")
+    outputFile = os.path.join(models_dir, "Evaluation results.txt")
+    sortedFiles = dict()
+    for model in os.listdir(models_dir):
+        spacy_model = spacy.load(os.path.join(models_dir, model))
+        results = evaluate(spacy_model, evalSents)
+        resultsStr = 'Model: %s\nResults:\n\tents_p:%s\n\tents_r:%s\n\tents_f:%s\n\tents_per_type:%s\n\n' % (
+            model, results['ents_p'], results['ents_r'], results['ents_f'], results['ents_per_type'])
+        b = model.split(' ')[0]
+        sortedFiles[b] = resultsStr
+        print("Evaluated %s" % model)
+    # getting a sorted list so i can print the results in natural order
+    sortedFileIdx = sorted(sortedFiles, key=int)
     with open(outputFile, mode='a', encoding='utf-8') as wf:
-        for model in models_dir:
-            results = evaluate(os.path.join(models_dir, model), evalSents)
-            resultsStr = 'Model: %s\nResults:\n\tents_p:%s\n\tents_r:%s\n\tents_f:%s\n\tents_per_type:%s\n\n' % (
-                    model, results['ents_p'], results['ents_r'], results['ents_f'], results['ents_per_type'])
-            wf.write(resultsStr)
-            print("Evaluated %s" % model)
+        for idx in sortedFileIdx:
+            wf.write(sortedFiles[idx])
 
 
 def makingEvaluationSents(input_bin):
@@ -61,5 +66,13 @@ def evaluate(ner_model, examples):
         scorer.score(pred_value, gold)
     return scorer.scores
 
+
 if __name__ == '__main__':
-    plac.call(main)
+    # plac.call(main)
+    """
+    pravilno povikuvanje (go zachuvuva rezultatot vo models dir)
+    python EvaluatingAModel -m "NER_models/general" -i "training_and_testing_data/deep_learning_model/testing.bin"
+    ili
+    python EvaluatingAModel -m "NER_models/specialized/activation funciton models" -i "training_and_testing_data/specialized_models/testing.bin"
+
+    """
