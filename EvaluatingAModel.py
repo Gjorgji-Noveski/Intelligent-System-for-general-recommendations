c93250246092c93e7ec70ea1071f5aa228a91f06
Gjorgji Noveski <37218494+Gjorgji-Noveski@users.noreply.github.com>
2021-07-29 00:51:33 +0200

    Refactoring

--- EvaluatingAModel.py
+++ EvaluatingAModel.py
@@ -1,29 +1,25 @@
 from spacy.gold import GoldParse
 from spacy.scorer import Scorer
+from os import path
+from os.path import join
 import os.path
 import pickle
 import plac
 import spacy
+import re
+import pandas as pd
 
-# TODO: Careful NOT to evaluate a model on the SAME sentences it was trained
-# With this function i'm making sentences in the format
-#     examples = [
-#     ('Who is Shaka Khan?',
-#      [(7, 17, 'PERSON')]),
-#     ('I like London and Berlin.',
-#      [(7, 13, 'LOC'), (18, 24, 'LOC')])
-# ]
 
 @plac.annotations(
-    models_dir=('Path to a directory where spacy NER_models will be used for evaluation', 'option', 'm', str),
-    input_file=('An input spacy binary file from which evaluation sentences are taken from', 'option', 'i', str)
+    models_dir=('Path to a directory where spacy NER_models will be used for evaluation', 'positional'),
+    input_file=('An input spacy binary file from which evaluation sentences are taken from', 'positional')
 )
 def main(models_dir=None, input_file=None):
     """
     This script writes evaluation results only for the NER pipeline of the model.
     It takes a directory containing sub directiories of many Spacy models and saves the evaluation results inside the
     models directory.
-    The binary input file is specified so the script know on which sentences to evaluate.
+    The binary input file is specified so the script knows on which sentences to evaluate.
     """
     if not os.path.isdir(models_dir):
         print('Please specify a directory to spacy NER_models')
@@ -32,21 +28,31 @@ def main(models_dir=None, input_file=None):
         print('Please specify an input file')
         raise SystemExit(1)
     evalSents = makingEvaluationSents(input_file)
-    outputFile = os.path.join(models_dir, "Evaluation results.txt")
-    sortedFiles = dict()
+    unsortedModels = dict()
+    header = 'epoch,ents_p,ents_r,ents_f\n'
     for model in os.listdir(models_dir):
         spacy_model = spacy.load(os.path.join(models_dir, model))
         results = evaluate(spacy_model, evalSents)
-        resultsStr = 'Model: %s\nResults:\n\tents_p:%s\n\tents_r:%s\n\tents_f:%s\n\tents_per_type:%s\n\n' % (
-            model, results['ents_p'], results['ents_r'], results['ents_f'], results['ents_per_type'])
-        b = model.split(' ')[0]
-        sortedFiles[b] = resultsStr
-        print("Evaluated %s" % model)
+        model = re.search(r'\d+', model).group()
+        results = '{epoch},{ents_p:.2f},{ents_r:.2f},{ents_f:.2f}\n'.format(epoch=model, ents_p=results['ents_p'],
+                                                                            ents_r=results['ents_r'],
+                                                                            ents_f=results['ents_f'])
+        unsortedModels[model] = results
+        # print("Evaluated %s" % model)
+
     # getting a sorted list so i can print the results in natural order
-    sortedFileIdx = sorted(sortedFiles, key=int)
+    sortedFileIdx = sorted(unsortedModels, key=int)
+    outputFile = os.path.join(models_dir, "Evaluation results.csv")
     with open(outputFile, mode='a', encoding='utf-8') as wf:
-        for idx in sortedFileIdx:
-            wf.write(sortedFiles[idx])
+        wf.write(header)
+        tus = [unsortedModels[i] for i in sortedFileIdx]
+        wf.writelines(tus)
+    df = pd.read_csv(outputFile)
+    bestEpochNm = str(int(df.iloc[df['ents_f'].argmax()][0]))
+    for model in os.listdir(models_dir):
+        if model.startswith(bestEpochNm):
+            print(path.abspath(join(models_dir, model)))
+            break
 
 
 def makingEvaluationSents(input_bin):
@@ -68,11 +74,4 @@ def evaluate(ner_model, examples):
 
 
 if __name__ == '__main__':
-    # plac.call(main)
-    """
-    pravilno povikuvanje (go zachuvuva rezultatot vo models dir)
-    python EvaluatingAModel -m "NER_models/general" -i "training_and_testing_data/deep_learning_model/testing.bin"
-    ili
-    python EvaluatingAModel -m "NER_models/specialized/activation funciton models" -i "training_and_testing_data/specialized_models/testing.bin"
-
-    """
+    plac.call(main)
